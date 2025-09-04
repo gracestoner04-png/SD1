a4f7a939d2abbe47ad569088ccc2869904655cac
gracestoner04-png <gracestoner04@gmail.com>
2025-09-04 13:25:09 -0400

    Update debug.java

--- Assignment 1/debug.java
+++ Assignment 1/debug.java
@@ -1,1 +1,32 @@
+//begin with importing scanner 
+import java.util.Scanner;
 
+public class Debug {
+    public static void main(String[]args){
+
+        //naming scanner for use
+        Scanner sc = new Scanner(System.in);
+
+        //first, prompting the user to imput their age
+        System.out.println("How old are you? ");
+
+        //second, assigning string to their age
+        String age = sc.nextLine();
+
+        //third, restating the users age back to them
+        System.out.println("You are, " + age + " years old! "); 
+
+        //assigning values to num1 and num2
+        int num1 = 10;
+        int num2 = 37;
+        //creating a variable for the sum of the 2 integers
+        int sum = num1+num2;
+
+        //printed sentence with the sum of 2 integers
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+        //"What I learned" - this was the first time that I have been introduced to using scanner in Java. 
+        //Also after the part of the assingment printing out the sum, I feel more confotable assigning values to strings and integers
+        //My biggest problem throughout this assingment was continuing to forget to add ";" at the end of each line and getting the same error over and over again :(
+    }
+}
